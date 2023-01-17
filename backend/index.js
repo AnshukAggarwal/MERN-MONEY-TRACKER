0c25845f5e01e48fc68d9029f774d3b3cbb3ac83
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2023-01-17 18:02:01 -0500

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const path = require("path");
 const userRouter = require("./Routes/userRoutes");
 const transactionsRouter = require("./Routes/transactionRoutes");
 const categoryRouter = require("./Routes/categoryRoutes");
@@ -15,6 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRouter);
 app.use("/api/transactions", transactionsRouter);
 app.use("/api/categories", categoryRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).send("Welcome");
+  });
+}
 
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 
